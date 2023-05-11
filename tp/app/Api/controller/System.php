0c2695d833ff530bd0f22cdbf31313ba8a82c304
yk <yk2199877358@163.com>
2023-05-11 16:17:46 +0800

    5月11

---
+++ tp/app/Api/controller/System.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace app\Api\controller;
+
+
+class System
+{
+    public function pushMsg()
+    {
+        echo '123';
+    }
+    public function config()
+    {
+
+    }
+    public function safeSee()
+    {
+
+    }
+    public function getUserFeeds()
+    {
+
+    }
+    public function getLoginLog()
+    {
+
+    }
+}
